fe690c2c6ff76b7e1d5f2aec23c8f2050525cd98
TheLove <lovepreetsinghcad@gmail.com>
2022-12-22 15:07:49 -0500

    binary search all test passed

--- binary-search.js
+++ binary-search.js
@@ -1,11 +1,36 @@
 function linearSearch (arr, target) {
 
-  // Can you solve this in one line?
-
+  // Can you solve this in one line? 
+  for(let i = 0 ;i < arr.length; i++){
+    if(arr[i] == target){
+      return i;
+    }
+  } 
+  return -1;
 };
 
 function binarySearch(arr, target) {
 
+  let lo = 0;
+  let hi = arr.length-1;
+  let mid = Math.floor((lo+hi)/2);
+
+  while(arr[mid] !== target){
+    if(lo > hi){
+      return -1;
+    }
+    else{
+      if(arr[mid] > target){
+        hi = mid - 1;
+        mid = Math.floor((lo+hi)/2); 
+        }
+        else{
+          lo = mid + 1;
+          mid  = Math.floor((lo+hi)/2);
+        }
+    }
+  }
+return mid;
   // Set integers pointing to the high and low range of possible indices
 
   // While high and low indices do not overlap...
